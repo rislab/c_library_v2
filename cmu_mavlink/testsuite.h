06d7023edafd7667378c1bf8c1ad6e45a305d4fe
Nathan Michael <nmichael@cmu.edu>
2017-08-13 16:53:56 -0400

    autogenerated headers for rev https://github.com/cmu-rasl/mavlink/tree/3f0457d9a545352a436099a2361f01884338c1e7

--- cmu_mavlink/testsuite.h
+++ cmu_mavlink/testsuite.h
@@ -12,17 +12,17 @@ extern "C" {
 
 #ifndef MAVLINK_TEST_ALL
 #define MAVLINK_TEST_ALL
-static void mavlink_test_standard(uint8_t, uint8_t, mavlink_message_t *last_msg);
+static void mavlink_test_common(uint8_t, uint8_t, mavlink_message_t *last_msg);
 static void mavlink_test_cmu_mavlink(uint8_t, uint8_t, mavlink_message_t *last_msg);
 
 static void mavlink_test_all(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
 {
-    mavlink_test_standard(system_id, component_id, last_msg);
+    mavlink_test_common(system_id, component_id, last_msg);
     mavlink_test_cmu_mavlink(system_id, component_id, last_msg);
 }
 #endif
 
-#include "../standard/testsuite.h"
+#include "../common/testsuite.h"
 
 
 static void mavlink_test_image_triggered_imu(uint8_t system_id, uint8_t component_id, mavlink_message_t *last_msg)
